2a2955ba652354bf472480bfe3683e5bb1aeabba
Micah Lindley <micahincorporated@gmail.com>
2022-10-02 21:53:25 -0500

    Support when caf hours have already started

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -4,10 +4,16 @@ import ft from "friendly-time";
 export default function Timer({ meal }) {
   return (
     <div className={styles.timerParent}>
-      {meal.start > new Date() && (
+      {meal.start >= new Date() && (
         <>
           <p>Next mealtime starts</p>
           <h2>{ft(new Date(meal.start))}</h2>
+        </>
+      )}
+      {meal.start < new Date() && (
+        <>
+          <p>This mealtime ends in</p>
+          <h2>{ft(new Date(meal.end))}</h2>
         </>
       )}
     </div>

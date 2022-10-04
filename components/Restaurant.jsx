64a4d843e35900676adef570db2677e5fb8fb2e6
Micah Lindley <hi@micahlindley.com>
2022-10-04 02:50:19 +0000

    Allow swapping between relative and exact times

--- components/Restaurant.jsx
+++ components/Restaurant.jsx
@@ -2,12 +2,26 @@ import styles from "../styles/Restaurant.module.css";
 
 export default function Restaurant({ restaurant: rr }) {
   return (
-    <div className={styles.restaurant} style={{backgroundImage: `url('${rr.image}')`, opacity: rr.hours.current ? 1 : 0.6}}>
+    <a
+      className={styles.restaurant}
+      style={{
+        backgroundImage: `url('${rr.image}')`,
+        opacity: rr.hours.current ? 1 : 0.6,
+      }}
+      href={rr.link}
+    >
       <h3 className={styles.name}>{rr.name}</h3>
       <h4>{rr.hours.current ? "Open" : "Closed"}</h4>
-      {rr.hours.open && (<p>{rr.hours.open} - {rr.hours.close}</p>)}
+      {rr.hours.open && (
+        <p>
+          {rr.hours.open} - {rr.hours.close}
+        </p>
+      )}
       {!rr.hours.open && <p>Not open today</p>}
-      <div className={styles.overlay} style={{backgroundColor: rr.hours.current ? rr.color : "#000"}}></div>
-    </div>
+      <div
+        className={styles.overlay}
+        style={{ backgroundColor: rr.hours.current ? rr.color : "#000" }}
+      ></div>
+    </a>
   );
 }
--- components/Timer.jsx
+++ components/Timer.jsx
@@ -1,21 +1,56 @@
 import styles from "../styles/Timer.module.css";
 import ft from "friendly-time";
+import { stringify as du } from "simple-duration";
+import { useEffect, useState } from "react";
 
 export default function Timer({ meal }) {
+  const [method, setMethod] = useState(null);
+  useEffect(() => {
+    if (localStorage.getItem("method")) {
+      setMethod(localStorage.getItem("method"));
+    } else {
+      setMethod("relative");
+      localStorage.setItem("method", "relative");
+    }
+  }, []);
+  const toggleMethod = () => {
+    if (method == "relative") {
+      setMethod("exact");
+      localStorage.setItem("method", "exact");
+    } else {
+      setMethod("relative");
+      localStorage.setItem("method", "relative");
+    }
+  };
   return (
-    <div className={styles.timerParent}>
-      {meal.start >= new Date() && (
-        <>
-          <p>Next mealtime starts</p>
-          <h2>{ft(new Date(meal.start))}</h2>
-        </>
+    <>
+      {method && (
+        <div className={styles.timerParent} onClick={toggleMethod}>
+          <span title="Swap time views" className={styles.timerSwap}>
+            swap_horiz
+          </span>
+          {meal.start >= new Date() && (
+            <>
+              <p>Next mealtime starts {method == "exact" && " in"}</p>
+              <h2>
+                {method == "relative"
+                  ? ft(new Date(meal.start))
+                  : du(meal.start / 1000 - Date.now() / 1000, "m")}
+              </h2>
+            </>
+          )}
+          {meal.start < new Date() && (
+            <>
+              <p>This mealtime ends</p>
+              <h2>
+                {method == "relative"
+                  ? ft(new Date(meal.end))
+                  : du(meal.end / 1000 - Date.now() / 1000, "m")}
+              </h2>
+            </>
+          )}
+        </div>
       )}
-      {meal.start < new Date() && (
-        <>
-          <p>This mealtime ends</p>
-          <h2>{ft(new Date(meal.end))}</h2>
-        </>
-      )}
-    </div>
+    </>
   );
 }

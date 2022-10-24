9016dde90a8ec8a0bfc31e993ff58ea24d4728e3
Micah Lindley <hi@micahlindley.com>
2022-10-24 14:48:43 +0000

    Correct GA snippet

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,10 @@
 import "../styles/globals.css";
-import Script from "next/script";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.CAFANALYID}`}
-      />
-
-      <Script strategy="afterInteractive" id="gascript">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.CAFANALYID}', {
-        page_path: window.location.pathname,
-        });
-    `}
-      </Script>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
   );

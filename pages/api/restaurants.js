513cdc1234a95a445514383359e5abb390d8b647
Micah Lindley <hi@micahlindley.com>
2022-11-30 06:39:11 +0000

    Go back to original CFA hours

--- pages/api/restaurants.js
+++ pages/api/restaurants.js
@@ -50,7 +50,7 @@ export default async function handler(req, res) {
       hours: {
         "M-T": {
           open: "10:30 AM",
-          close: "3:30 PM",
+          close: "6:00 PM",
         },
         F: {
           open: "10:30 AM",

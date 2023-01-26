9134979133e285497c008d7cbf9aa3dbdefdeecf
Micah Lindley <micahincorporated@gmail.com>
2023-01-26 02:34:52 -0600

    Add privacy page

---
+++ pages/privacy.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import s from "../styles/Home.module.css";
+import a from "../styles/Admin.module.css";
+import Link from "next/link";
+
+export default function Privacy() {
+  return (
+    <div className={s.container}>
+      <Head>
+        <title>Privacy | The Caf at MC</title>
+        <meta
+          name="description"
+          content="Admin page to submit alerts to TheCaf.me"
+        />
+        <link rel="icon" href="/icons/icon.png" />
+      </Head>
+      <main className={s.main}>
+        <header className={s.header}>
+          <Link href="/">
+            <img src="/back.svg" />
+          </Link>
+          <h1>Privacy</h1>
+        </header>
+        <div className={s.content}>
+          <div className={a.form}>
+            <h2>Peace of mind.</h2>
+            <p style={{ lineHeight: "1.3em" }}>
+              TheCaf.me stores as little user data as possible. The only thing
+              stored in our private database is your display name (from your
+              Google account), your point value, and your unique ID (also
+              connected to Google). This information is only stored and accessed
+              on TheCaf.me and is not sold to third parties.
+              <br />
+              <br />
+              Google grants us access to your email, profile picture, and other
+              personal data. We do not share or transmit that data anywhere
+              except on TheCaf.me.
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}

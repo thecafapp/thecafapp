df98ac02f88dc825f31745efe433f2a4ee8013d2
Micah Lindley <hi@micahlindley.com>
2023-09-27 21:03:48 +0000

    💄 Hide intermediate state for point tracker

--- components/PointTracker.jsx
+++ components/PointTracker.jsx
@@ -7,6 +7,7 @@ import useFirebaseUser from "../hooks/useFirebaseUser";
 
 export default function PointTracker() {
   const [balance, setBalance] = useState("--.--");
+  const [loaded, setLoaded] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [modalScreen, setModalScreen] = useState("auto");
   const user = useFirebaseUser();
@@ -17,6 +18,7 @@ export default function PointTracker() {
     if (!user) {
       setBalance("--.--");
       setModalScreen("manual");
+      setLoaded(true);
       return;
     }
     fetch(`/api/balance`, {
@@ -35,6 +37,7 @@ export default function PointTracker() {
           setModalScreen("manual");
           setBalance("--.--");
         }
+        setLoaded(true);
       });
   };
   useEffect(() => {
@@ -42,60 +45,62 @@ export default function PointTracker() {
   }, [user]);
   return (
     <>
-      <div className={styles.tracker}>
-        <h4>Point Tracker</h4>
-        <h2 className={styles.remaining}>
+      {loaded && (
+        <div className={styles.tracker}>
+          <h4>Point Tracker</h4>
+          <h2 className={styles.remaining}>
+            <span
+              className={styles.pointValue}
+              title="Click to edit your points"
+              onClick={() => setModalIsOpen(true)}
+            >
+              ${balance}
+            </span>{" "}
+            points remaining
+          </h2>
+          {balance != "0.00" && balance != "--.--" ? (
+            <>
+              <p className={styles.usedFor}>
+                <span className={styles.usedForHeading}>
+                  That&apos;s enough for:
+                </span>
+              </p>
+              <ul>
+                <li>
+                  <b>{enoughFor(9.19)}</b> Chick-Fil-A Sandwich Meals
+                </li>
+                <li>
+                  <b>{enoughFor(4.19)}</b> Chick-Fil-A Milkshakes
+                </li>
+                <li>
+                  <b>{enoughFor(3.25)}</b> Starbucks Caffe Lattes
+                </li>
+                <li>
+                  <b>{enoughFor(5.0)}</b> Einstein&apos;s Regular Bagels
+                </li>
+              </ul>
+            </>
+          ) : (
+            <a
+              href="#"
+              className={styles.getStarted}
+              onClick={(e) => {
+                e.preventDefault();
+                setModalIsOpen(true);
+              }}
+            >
+              Get started with point tracking
+            </a>
+          )}
           <span
-            className={styles.pointValue}
             title="Click to edit your points"
+            className={styles.edit}
             onClick={() => setModalIsOpen(true)}
           >
-            ${balance}
-          </span>{" "}
-          points remaining
-        </h2>
-        {balance != "0.00" && balance != "--.--" ? (
-          <>
-            <p className={styles.usedFor}>
-              <span className={styles.usedForHeading}>
-                That&apos;s enough for:
-              </span>
-            </p>
-            <ul>
-              <li>
-                <b>{enoughFor(9.19)}</b> Chick-Fil-A Sandwich Meals
-              </li>
-              <li>
-                <b>{enoughFor(4.19)}</b> Chick-Fil-A Milkshakes
-              </li>
-              <li>
-                <b>{enoughFor(3.25)}</b> Starbucks Caffe Lattes
-              </li>
-              <li>
-                <b>{enoughFor(5.0)}</b> Einstein&apos;s Regular Bagels
-              </li>
-            </ul>
-          </>
-        ) : (
-          <a
-            href="#"
-            className={styles.getStarted}
-            onClick={(e) => {
-              e.preventDefault();
-              setModalIsOpen(true);
-            }}
-          >
-            Get started with point tracking
-          </a>
-        )}
-        <span
-          title="Click to edit your points"
-          className={styles.edit}
-          onClick={() => setModalIsOpen(true)}
-        >
-          edit
-        </span>
-      </div>
+            edit
+          </span>
+        </div>
+      )}
       <Modal
         isOpen={modalIsOpen}
         className={modalStyles.alertModal}

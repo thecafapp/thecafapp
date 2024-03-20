59612f9ff8fa0af07beb87574933026bf26a62ef
Micah Lindley <hi@micahlindley.com>
2024-03-20 10:15:57 -0500

    Also daylight savings

--- pages/api/restaurants.js
+++ pages/api/restaurants.js
@@ -102,7 +102,7 @@ export default async function handler(req, res) {
           ", " +
           time +
           " CST"
-      ) - 0 // add 3600000 for Daylight Savings, 0 for not
+      ) - 3600000 // add 3600000 for Daylight Savings, 0 for not
     );
   };
   const json = { restaurants: [] };

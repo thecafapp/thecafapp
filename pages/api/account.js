52d86780f4a01aba8f0406109b19e4eaac57ba89
Micah Lindley <hi@micahlindley.com>
2024-08-19 13:55:44 -0500

    Add temporary maintenence message

--- pages/api/account.js
+++ pages/api/account.js
@@ -65,7 +65,6 @@ export default async function handler(req, res) {
       });
     }
   } else if (req.method == "POST") {
-    console.log("\n\n", req.headers, "\n\n");
     if (!req.headers["x-firebase-token"]) {
       await client.close();
       await firebaseApp.delete();

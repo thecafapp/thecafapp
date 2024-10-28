a2caad3571503c9361763b7fe6453df33ad6ee43
Micah Lindley <hi@micahlindley.com>
2024-10-28 14:58:31 -0500

    Fix Next build errors with new layout feature

--- components/layout/LayoutItem.jsx
+++ components/layout/LayoutItem.jsx
@@ -1,8 +1,11 @@
 import React, { forwardRef } from 'react';
 import s from "../../styles/LayoutItem.module.css";
 
-export default forwardRef(({ id, name, ...props }, ref) => {
+const LayoutItem = forwardRef(({ id, name, ...props }, ref) => {
   return (
     <div {...props} className={s.item} ref={ref}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg> {name}</div>
   )
 });
+LayoutItem.displayName = "LayoutItem";
+
+export default LayoutItem;

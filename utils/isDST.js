b212a89f721c035b7b3802eb94f42243c8ac6d26
Micah Lindley <hi@micahlindley.com>
2025-03-17 11:36:08 -0500

    Fix DST for real this time

--- utils/isDST.js
+++ utils/isDST.js
@@ -1,7 +1,15 @@
+function getTimezoneOffset(atTime, timeZone) {
+    const localizedTime = new Date(atTime.toLocaleString("en-US", { timeZone }));
+    const utcTime = new Date(atTime.toLocaleString("en-US", { timeZone: "UTC" }));
+    return Math.round((localizedTime.getTime() - utcTime.getTime()) / (60 * 1000));
+}
+
 const isDST = (d = new Date()) => {
-    let jan = new Date(d.getFullYear(), 0, 1).getTimezoneOffset();
-    let jul = new Date(d.getFullYear(), 6, 1).getTimezoneOffset();
-    return Math.max(jan, jul) !== d.getTimezoneOffset();
+    let jan = getTimezoneOffset(new Date(d.getFullYear(), 0, 1), "America/Chicago");
+    let jul = getTimezoneOffset(new Date(d.getFullYear(), 6, 1), "America/Chicago");
+    console.log(jan, jul);
+    console.log(getTimezoneOffset(d, "America/Chicago"));
+    return Math.max(jan, jul) === getTimezoneOffset(d, "America/Chicago");
 }
 
 export default isDST;
